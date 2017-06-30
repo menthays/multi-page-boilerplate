172711b3032f9fa01c383f1e8206a57c2c18333f
yang.hao <yang.hao@igarment.net>
2017-06-30 17:06:52 +0800

    add some illustration

--- src/routes/index/App.vue
+++ src/routes/index/App.vue
@@ -1,6 +1,9 @@
 <template lang="html">
 <div>
   <h2>{{msg}}</h2>
+  <p>1. Delete .git</p>
+  <p>2. Add new pages in 'routes/'</p>
+  <p>3. Start your projects!</p>
   <g-welcome></g-welcome>
 </div>
 </template>
